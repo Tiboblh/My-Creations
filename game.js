82199102ae1d705b6d0ff42003fb0519ba7e689e
Tiboblh <110022950+Tiboblh@users.noreply.github.com>
2025-02-14 13:25:32 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
 function start() {
-    document.body.innerHTML = "<h1 style='text-align: center; font-size: 50px; color: red;'>Tu as été hacké!</h1>";
-
+    document.body.innerHTML = "<h1 style='text-align: center; font-size: 50px; color: red;'>Tu as été hacké!</h1> <h1 style='text-align: center; font-size: 50px; color: black;>R.I.P</h1>";
+    
 }
